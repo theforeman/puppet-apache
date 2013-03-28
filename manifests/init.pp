5946bf3f3c32dbacc4423783ba9d9fa46d26741e
Byron Miller <bmiller@luminexcorp.com>
2013-03-28 10:11:59 -0500

    fixes #2342 keeping $oslevel, will require facter rpm

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,25 +1,4 @@
 class apache {
-  
-  if ! $::osfamily {
-  case $::operatingsystem {
-    'RedHat', 'Fedora', 'CentOS', 'Scientific', 'SLC', 'Ascendos', 'CloudLinux', 'PSBM', 'OracleLinux', 'OVS', 'OEL': {
-      $osfamily = 'RedHat'
-    }
-    'ubuntu', 'debian': {
-      $osfamily = 'Debian'
-    }
-    'SLES', 'SLED', 'OpenSuSE', 'SuSE': {
-      $osfamily = 'Suse'
-    }
-    'Solaris', 'Nexenta': {
-      $osfamily = 'Solaris'
-    }
-    default: {
-      $osfamily = $::operatingsystem
-    }
-   }
-  }
-
   include apache::params
   include apache::install
   include apache::config
